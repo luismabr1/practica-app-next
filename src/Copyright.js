d1db08dcd8ec79fc3c77ae43b2fa65301ba16107
Luis Manuel Brito <lsbrt4@gmail.com>
2021-03-11 15:27:37 -0400

    Cambio en el footer

--- src/Copyright.js
+++ src/Copyright.js
@@ -7,7 +7,7 @@ export default function Copyright() {
     <Typography variant="body2" color="inherit" align="center">
       {'Copyright © '}
       <MuiLink color="inherit" href="https://material-ui.com/">
-        Your Website
+        Movenu internacional
       </MuiLink>{' '}
       {new Date().getFullYear()}.
     </Typography>
